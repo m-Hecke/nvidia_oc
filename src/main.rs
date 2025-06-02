b260e774d8e5ac160842011c440d34cedbedb79d
DreamingCodes <me@dreaming.codes>
2025-06-02 21:42:14 +0200

    stop using nvml-wrapper-sys removing unsafe methods

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
 use clap::{arg, Args, CommandFactory, Parser, Subcommand};
 use clap_complete::{generate, Generator, Shell};
-use nvml_wrapper::{
-    error::{nvml_try, NvmlError},
-    Nvml,
-};
-use nvml_wrapper_sys::bindings::{nvmlDevice_t, nvmlReturn_t, NvmlLib};
+use nvml_wrapper::{Device, Nvml};
 use serde::Deserialize;
 use std::{collections::HashMap, io};
 
@@ -71,29 +67,40 @@ struct Sets {
 }
 
 impl Sets {
-    fn apply(&self, nvml: &NvmlLib, device: nvmlDevice_t) {
+    fn apply(&self, device: &mut Device) {
         if let Some(freq_offset) = self.freq_offset {
-            set_gpu_frequency_offset(&nvml, device, freq_offset)
+            device
+                .set_gpc_clock_vf_offset(freq_offset)
                 .expect("Failed to set GPU frequency offset");
         }
 
         if let Some(mem_offset) = self.mem_offset {
-            set_gpu_memory_frequency_offset(&nvml, device, mem_offset)
+            device
+                .set_mem_clock_vf_offset(mem_offset)
                 .expect("Failed to set GPU memory frequency offset");
         }
 
         if let Some(limit) = self.power_limit {
-            set_gpu_power_limit(&nvml, device, limit).expect("Failed to set GPU power limit");
+            device
+                .set_power_management_limit(limit)
+                .expect("Failed to set GPU power limit");
         }
 
         if let (Some(min_clock), Some(max_clock)) = (self.min_clock, self.max_clock) {
-            set_gpu_min_max_clock(&nvml, device, min_clock, max_clock)
+            device
+                .set_gpu_locked_clocks(
+                    nvml_wrapper::enums::device::GpuLockedClocksSetting::Numeric {
+                        min_clock_mhz: min_clock,
+                        max_clock_mhz: max_clock,
+                    },
+                )
                 .expect("Failed to set GPU min and max clocks");
         }
 
         if let (Some(min_mem_clock), Some(max_mem_clock)) = (self.min_mem_clock, self.max_mem_clock)
         {
-            set_gpu_min_max_mem_clock(&nvml, device, min_mem_clock, max_mem_clock)
+            device
+                .set_mem_locked_clocks(min_mem_clock, max_mem_clock)
                 .expect("Failed to set GPU min and max memory clocks");
         }
     }
@@ -118,46 +125,31 @@ fn main() {
 
             let nvml = Nvml::init().expect("Failed to initialize NVML");
 
-            let device = nvml.device_by_index(*index).expect("Failed to get GPU");
+            let mut device = nvml.device_by_index(*index).expect("Failed to get GPU");
 
-            unsafe {
-                let raw_device_handle: nvmlDevice_t = device.handle();
-                let nvml_lib =
-                    NvmlLib::new("libnvidia-ml.so").expect("Failed to load NVML library");
-
-                sets.apply(&nvml_lib, raw_device_handle);
-            }
+            sets.apply(&mut device);
             println!("Successfully set GPU parameters.");
         }
         Some(Commands::Get { index }) => {
             let nvml = Nvml::init().expect("Failed to initialize NVML");
             let device = nvml.device_by_index(*index).expect("Failed to get GPU");
 
-            unsafe {
-                let raw_device_handle: nvmlDevice_t = device.handle();
-                let nvml_lib =
-                    NvmlLib::new("libnvidia-ml.so").expect("Failed to load NVML library");
+            let freq_offset = device.gpc_clock_vf_offset();
+            match freq_offset {
+                Ok(freq_offset) => println!("GPU core clock offset: {} MHz", freq_offset),
+                Err(e) => eprintln!("Failed to get GPU core clock offset: {:?}", e),
+            }
 
-                let freq_offset =
-                    get_value(|v| nvml_lib.nvmlDeviceGetGpcClkVfOffset(raw_device_handle, v));
-                match freq_offset {
-                    Ok(freq_offset) => println!("GPU core clock offset: {} MHz", freq_offset),
-                    Err(e) => eprintln!("Failed to get GPU core clock offset: {:?}", e),
-                }
+            let mem_offset = device.mem_clock_vf_offset();
+            match mem_offset {
+                Ok(mem_offset) => println!("GPU memory clock offset: {} MHz", mem_offset),
+                Err(e) => eprintln!("Failed to get GPU memory clock offset: {:?}", e),
+            }
 
-                let mem_offset =
-                    get_value(|v| nvml_lib.nvmlDeviceGetMemClkVfOffset(raw_device_handle, v));
-                match mem_offset {
-                    Ok(mem_offset) => println!("GPU memory clock offset: {} MHz", mem_offset),
-                    Err(e) => eprintln!("Failed to get GPU memory clock offset: {:?}", e),
-                }
-
-                let power_limit =
-                    get_value(|v| nvml_lib.nvmlDeviceGetEnforcedPowerLimit(raw_device_handle, v));
-                match power_limit {
-                    Ok(power_limit) => println!("GPU power limit: {} W", power_limit / 1000),
-                    Err(e) => eprintln!("Failed to get GPU power limit: {:?}", e),
-                }
+            let power_limit = device.enforced_power_limit();
+            match power_limit {
+                Ok(power_limit) => println!("GPU power limit: {} W", power_limit / 1000),
+                Err(e) => eprintln!("Failed to get GPU power limit: {:?}", e),
             }
         }
         None => {
@@ -172,14 +164,9 @@ fn main() {
 
             let nvml = Nvml::init().expect("Failed to initialize NVML");
 
-            unsafe {
-                let nvml_lib =
-                    NvmlLib::new("libnvidia-ml.so").expect("Failed to load NVML library");
-
-                for (index, sets) in config.sets {
-                    let device = nvml.device_by_index(index).expect("Failed to get GPU");
-                    sets.apply(&nvml_lib, device.handle());
-                }
+            for (index, sets) in config.sets {
+                let mut device = nvml.device_by_index(index).expect("Failed to get GPU");
+                sets.apply(&mut device);
             }
             println!("Successfully set GPU parameters.");
         }
@@ -205,83 +192,6 @@ fn escalate_permissions() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     Ok(())
-}
-
-fn get_value<T, F>(f: F) -> Result<T, Option<NvmlError>>
-where
-    T: Default,
-    F: FnOnce(*mut T) -> nvmlReturn_t,
-{
-    let mut value = T::default();
-    let status = f(&mut value);
-    if status == 0 {
-        Ok(value)
-    } else {
-        Err(nvml_try(status).err())
-    }
-}
-
-fn set_gpu_frequency_offset(
-    nvml_lib: &NvmlLib,
-    handle: nvmlDevice_t,
-    offset: i32,
-) -> Result<(), String> {
-    let result = unsafe { nvml_lib.nvmlDeviceSetGpcClkVfOffset(handle, offset) };
-    if result != 0 {
-        Err(format!("Error code: {}", result))
-    } else {
-        Ok(())
-    }
-}
-
-fn set_gpu_memory_frequency_offset(
-    nvml_lib: &NvmlLib,
-    handle: nvmlDevice_t,
-    offset: i32,
-) -> Result<(), String> {
-    let result = unsafe { nvml_lib.nvmlDeviceSetMemClkVfOffset(handle, offset) };
-    if result != 0 {
-        Err(format!("Error code: {}", result))
-    } else {
-        Ok(())
-    }
-}
-
-fn set_gpu_power_limit(nvml_lib: &NvmlLib, handle: nvmlDevice_t, limit: u32) -> Result<(), String> {
-    let result = unsafe { nvml_lib.nvmlDeviceSetPowerManagementLimit(handle, limit) };
-    if result != 0 {
-        Err(format!("Error code: {}", result))
-    } else {
-        Ok(())
-    }
-}
-
-fn set_gpu_min_max_clock(
-    nvml_lib: &NvmlLib,
-    handle: nvmlDevice_t,
-    minclock: u32,
-    maxclock: u32,
-) -> Result<(), String> {
-    let result = unsafe { nvml_lib.nvmlDeviceSetGpuLockedClocks(handle, minclock, maxclock) };
-    if result != 0 {
-        Err(format!("Error code: {}", result))
-    } else {
-        Ok(())
-    }
-}
-
-fn set_gpu_min_max_mem_clock(
-    nvml_lib: &NvmlLib,
-    handle: nvmlDevice_t,
-    minclock: u32,
-    maxclock: u32,
-) -> Result<(), String> {
-    let result = unsafe { nvml_lib.nvmlDeviceSetMemoryLockedClocks(handle, minclock, maxclock) };
-    if result != 0 {
-        Err(format!("Error code: {}", result))
-    } else {
-        Ok(())
-    }
 }
 
 fn generate_completion_script<G: Generator>(gen: G) {
